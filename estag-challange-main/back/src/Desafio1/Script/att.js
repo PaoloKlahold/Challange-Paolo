e85864727b58056c6df40a9141ddb711d17261e2
Paolo Klahold <105084677+PaoloKlahold@users.noreply.github.com>
2024-03-14 14:30:22 -0300

    Delete estag-challange-main directory

--- estag-challange-main/back/src/Desafio1/Script/att.js
+++
@@ -1,9 +0,0 @@
-function att(){
-    console.log('chegou');
-    
-    console.log('Valor: ' + document.getElementById('dropdownlistProduct').value)
-    price = document.getElementById(document.getElementById('dropdownlistProduct').value +'price').value
-    tax = document.getElementById(document.getElementById('dropdownlistProduct').value +'tax').value
-    $('#TaxInput').val(tax);   
-    $('#UnitInput').val(price);
-}
--- estag-challange-main/back/src/Desafio1/Script/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION['Acess'] = true;
-$_SESSION['ActualUser'] = '';
-?>
-<script>
-        window.location.href = "../cadastro.php";  
-
-</script>
